a12a1a4ab28c2b11fed62fa74755d077a24e0984
Kenichi Saito <hajime@studiohff.net>
2017-12-22 00:48:23 +0900

    タブの位置を変更

--- keyboards/ergodox_ez/keymaps/hajimepg/keymap.c
+++ keyboards/ergodox_ez/keymaps/hajimepg/keymap.c
@@ -28,9 +28,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |   @    |   1  |   2  |   3  |   4  |   5  |  ¥   |           | Meh  |   6  |   7  |   8  |   9  |   0  |   [    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | Del    |   Q  |   W  |   E  |   R  |   T  | Alf  |           |  L1  |   Y  |   U  |   I  |   O  |   P  |   ]    |
+ * | Tab    |   Q  |   W  |   E  |   R  |   T  | Alf  |           |  L1  |   Y  |   U  |   I  |   O  |   P  |   ]    |
  * |--------+------+------+------+------+------| red  |           |      |------+------+------+------+------+--------|
- * | Tab    |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / L2|   :    |
+ * | Del    |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / L2|   :    |
  * |--------+------+------+------+------+------| Hyper|           |  -   |------+------+------+------+------+--------|
  * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl|   _    |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
@@ -49,8 +49,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
         JKC_AT,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_JYEN,
-        KC_DELT,        KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   LALT(KC_SPC),
-        KC_TAB,         KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
+        KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   LALT(KC_SPC),
+        KC_DELT,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        CTL_T(KC_Z),  KC_X,   KC_C,   KC_V,   KC_B,   ALL_T(KC_NO),
         KC_LCTRL,       KC_LALT,      KC_LGUI,        KC_LEFT,KC_RGHT,
                                               ALT_T(KC_APP),  KC_LGUI,
