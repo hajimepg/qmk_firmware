b75169d3fc4534b00767d3902767a4e00924df13
Kenichi Saito <hajime@studiohff.net>
2017-12-21 23:24:59 +0900

    十字キー

--- keyboards/ergodox_ez/keymaps/hajimepg/keymap.c
+++ keyboards/ergodox_ez/keymaps/hajimepg/keymap.c
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
  * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl|   _    |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | LCtrl| LAlt |LGui  | Left | Right|                                       |  Up  | Down |   [  |   ]  | ~L1  |
+ *   | LCtrl| LAlt |LGui  | Left | Right|                                       |  Up  | Left |  Up  | Down | Right |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | App  | LGui |       | Alt  |Ctrl/Esc|
@@ -59,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              TG(SYMB),    KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             JKC_RBR,
                           KC_H,   KC_J,  KC_K,   KC_L,   LT(MDIA, KC_SCLN),JKC_CLN,
              MEH_T(KC_NO),KC_N,   KC_M,  KC_COMM,KC_DOT, CTL_T(KC_SLSH),   JKC_UNS,
-                                  KC_UP, KC_DOWN,KC_LBRC,KC_RBRC,          KC_FN1,
+                                  KC_UP, KC_LEFT,KC_UP,  KC_DOWN,          KC_RGHT,
              KC_LALT,        CTL_T(KC_ESC),
              KC_PGUP,
              KC_PGDN,KC_ENT, KC_SPC
