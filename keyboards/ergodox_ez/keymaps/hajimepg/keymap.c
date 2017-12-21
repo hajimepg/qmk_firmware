9f15628737b83caaf51604c023841e177b049144
Kenichi Saito <hajime@studiohff.net>
2017-12-22 00:15:40 +0900

    左手に左右矢印、右手に上下矢印

--- keyboards/ergodox_ez/keymaps/hajimepg/keymap.c
+++ keyboards/ergodox_ez/keymaps/hajimepg/keymap.c
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
  * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl|   _    |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | LCtrl| LAlt | LGui | Left | Right|                                       |  Up  | Left |  Up  | Down | Right |
+ *   | LCtrl| LAlt | LGui | Left | Right|                                       |  Up  | Down |  Up  | Down | Right |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | App  | LGui |       | Alt  |Ctrl/Esc|
@@ -60,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              TG(SYMB),    KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             JKC_RBR,
                           KC_H,   KC_J,  KC_K,   KC_L,   LT(MDIA, KC_SCLN),JKC_CLN,
              MEH_T(KC_NO),KC_N,   KC_M,  KC_COMM,KC_DOT, CTL_T(KC_SLSH),   JKC_UNS,
-                                  KC_UP, KC_LEFT,KC_UP,  KC_DOWN,          KC_RGHT,
+                                  KC_UP, KC_DOWN,KC_UP,  KC_DOWN,          KC_RGHT,
              KC_LALT,        CTL_T(KC_ESC),
              KC_PGUP,
              KC_PGDN,KC_ENT, KC_SPC
