98085a1942f336ec44819d505b000828d831c472
Kenichi Saito <hajime@studiohff.net>
2017-12-27 05:49:25 +0900

    レイヤー1の名前をSymbolsからFunctionに変更

--- keyboards/ergodox_ez/keymaps/hajimepg/keymap.c
+++ keyboards/ergodox_ez/keymaps/hajimepg/keymap.c
@@ -4,7 +4,7 @@
 #include "version.h"
 
 #define BASE 0 // default layer
-#define SYMB 1 // symbols
+#define FN   1 // function keys
 #define MDIA 2 // media keys
 
 // Japanese Key Code
@@ -53,12 +53,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTRL,       KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        CTL_T(KC_Z),  KC_X,   KC_C,   KC_V,   KC_B,   ALL_T(KC_NO),
         KC_LCTRL,       KC_LALT,      KC_LGUI,        KC_LEFT,KC_RGHT,
-                                                    MO(SYMB), KC_LGUI,
+                                                    MO(FN),   KC_LGUI,
                                                               KC_HOME,
                                                KC_BSPC,KC_BSPC,KC_LANG1,
         // right hand
              MEH_T(KC_NO),KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             JKC_LBR,
-             TG(SYMB),    KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             JKC_RBR,
+             TG(FN),      KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             JKC_RBR,
                           KC_H,   KC_J,   KC_K,   KC_L,   LT(MDIA, KC_SCLN),JKC_CLN,
              KC_MINS,     KC_N,   KC_M,   KC_COMM,KC_DOT, KC_UP,  JKC_UNS,
                                   KC_SLSH,JKC_HAT,KC_LEFT,KC_DOWN,KC_RGHT,
@@ -66,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              KC_PGUP,
              KC_LANG2,KC_ENT, KC_SPC
     ),
-/* Keymap 1: Symbol Layer
+/* Keymap 1: Function Layer
  *
  * ,---------------------------------------------------.           ,--------------------------------------------------.
  * | Reset   |  F1  |  F2  |  F3  |  F4  |  F5  |      |           |      |  F6  |  F7  |  F8  |  F9  |  F10 |   F11  |
@@ -87,8 +87,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                 |      |      |      |       |      |      |      |
  *                                 `--------------------'       `--------------------'
  */
-// SYMBOLS
-[SYMB] = LAYOUT_ergodox(
+// Function
+[FN] = LAYOUT_ergodox(
        // left hand
        RESET,  KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_TRNS,
        KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
@@ -152,7 +152,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 const uint16_t PROGMEM fn_actions[] = {
-    [1] = ACTION_LAYER_TAP_TOGGLE(SYMB)                // FN1 - Momentary Layer 1 (Symbols)
+    [1] = ACTION_LAYER_TAP_TOGGLE(FN)                  // FN1 - Momentary Layer 1 (Functions)
 };
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
