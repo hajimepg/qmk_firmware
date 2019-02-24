20ce9a3818373139e6131c8d103cb01ed5098f35
Kenichi Saito <hajime@studiohff.net>
2019-02-24 22:20:53 +0900

    LEDの色を青から緑に変更

--- keyboards/ergodox_ez/keymaps/hajipy/keymap.c
+++ keyboards/ergodox_ez/keymaps/hajipy/keymap.c
@@ -249,7 +249,7 @@ uint32_t layer_state_set_user(uint32_t state) {
 #ifdef RGB_MATRIX_ENABLE
 void rgb_matrix_indicators_user(void) {
   for (int i = 0; i < DRIVER_LED_TOTAL; i++) {
-    rgb_matrix_set_color(i, 0x00, 0x00, 0xFF);
+    rgb_matrix_set_color(i, 0x00, 0xFF, 0x00);
   }
 }
 #endif
